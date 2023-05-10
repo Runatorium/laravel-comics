65c7bc4f0f7542c35c66e414d0063b53c71dd62a
Runatorium <stefano.alesci1@gmail.com>
2023-05-10 16:41:00 +0200

    Working on Routes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+
+@section('page-title')
+    <h1>PAGINA PRINCIPALE</h1>
+@endsection
+
+
+@section('content')
+    <h1>bencenuto sul sito della comics Dc</h1>
+@endsection
